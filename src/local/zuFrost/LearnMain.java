3279f32158d890708b42dfe8598a604b9112ec8c
Alex Frost <zufrost.a@gmail.com>
2020-04-14 00:41:01 +0300

    add Exercise to programm

--- src/local/zuFrost/LearnMain.java
+++ src/local/zuFrost/LearnMain.java
@@ -6,5 +6,15 @@ public class LearnMain {
         System.out.println(42);
         System.out.println("\n\tjava " + 11);
 
+        int i = 10;
+        if(i < 10) {
+            System.out.print(1);
+        } else if (i <= 10) {
+            System.out.print(2);
+        }
+        System.exit(i);
+        System.out.print(3);
+
+
     }
 }
