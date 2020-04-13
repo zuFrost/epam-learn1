5dc82e4492e32195882dc50ed8499150b1cd3718
Alex Frost <zufrost.a@gmail.com>
2020-04-14 00:30:08 +0300

    make 3 kind of println

---
+++ src/local/zuFrost/LearnMain.java
@@ -0,0 +1,10 @@
+package local.zuFrost;
+
+public class LearnMain {
+    public static void main(String[] args) {
+        System.out.println("hello Java");
+        System.out.println(42);
+        System.out.println("\n\tjava " + 11);
+
+    }
+}
